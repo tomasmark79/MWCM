66b8db771ee174cee8c4d06a5c5b0fefee12294d
tomasmark79 <tomas@digitalspace.name>
2025-01-11 11:51:09 +0100

    added oatpp & libcurl

--- conanfile.py
+++ conanfile.py
@@ -10,8 +10,9 @@ class CompressorRecipe(ConanFile):
         self.requires("zlib/1.2.11")
         self.requires("fmt/11.0.2")
         self.requires("nlohmann_json/3.11.2")
-        #self.requires("libcurl/8.10.1")
-    
+        self.requires("oatpp/1.3.0.latest")
+        self.requires("libcurl/8.10.1")
+            
     #def build_requirements(self):
         #self.tool_requires("cmake/3.22.6")
 
